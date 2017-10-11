d167f4d3fb3b9166b6422e2ee506e8ba218aa68f
Bohdan <bohdan.benkevych@gmail.com>
2017-10-11 17:58:45 +0300

    Side menu active link fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import { RouterModule } from '@angular/router';
+import { RouterModule, RouterLinkActive } from '@angular/router';
 
 import { appRoutes } from './app.routes';
 
